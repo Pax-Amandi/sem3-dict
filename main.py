7d43da396e5c5ed195f7cfd45dfe6d65207b4187
Pax-Amandi <9334590+Pax-Amandi@users.noreply.github.com>
2018-02-14 11:47:29 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+__author__ = ""
+__copyright__ = ""
+__email__ = ""
+
+
+
+def dict(list1, list2):
+    """
+    
+    docstring располагается здесь 
+    
+    """
+
+    # ваш код располагается ниже этой строки 
+    pass  # pass можно удалить
+
+    # return
+
+
+if __name__ == '__main__':
+	# тесты располагаются ниже этой строки 
+	pass  # pass можно удалить
